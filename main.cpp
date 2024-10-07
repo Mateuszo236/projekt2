079a53f3ad29c8dca03690d2c9f35b7d65296c96
Mateuszo236 <mateuszoselkowski@gmail.com>
2024-10-07 22:05:24 +0200

    Dodawanie minimalnej i maksymalnej wartości dla INT

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
-int main(){
-    cout << "Hello, World!" <<endl;
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "Minimalna wartość dla int: " << numeric_limits<int>::min() << '\n';
+    cout << "Maksymalna wartość dla int: " << numeric_limits<int>::max() << '\n';
     return 0;
 }
